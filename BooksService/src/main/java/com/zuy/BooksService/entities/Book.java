3b1c4f72e1ef7ed1351e0a0bb732b22082ba3be4
Ckt2002 <toan652002@gmail.com>
2024-04-14 16:04:18 +0700

    UpdateDiagram

--- BooksService/src/main/java/com/zuy/BooksService/entities/Book.java
+++ BooksService/src/main/java/com/zuy/BooksService/entities/Book.java
@@ -16,4 +16,68 @@ public class Book {
     private String publishDate;
     private int quantity;
     private int availableQuantity;
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public void setPublishDate(String publishDate) {
+        this.publishDate = publishDate;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setAvailableQuantity(int availableQuantity) {
+        this.availableQuantity = availableQuantity;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public String getPublishDate() {
+        return publishDate;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public int getAvailableQuantity() {
+        return availableQuantity;
+    }
 }
