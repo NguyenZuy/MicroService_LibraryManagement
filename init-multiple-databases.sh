7227381cea1cc7e8339167d71e44b145ec15b0f2
NguyenDuy <89451574+MisYuy@users.noreply.github.com>
2024-06-11 21:14:01 +0700

    Add Docker

---
+++ init-multiple-databases.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+    CREATE DATABASE book;
+    CREATE DATABASE loan_slip;
+    CREATE DATABASE return_slip;
+    CREATE DATABASE supplier;
+    CREATE DATABASE order_slip;
+    CREATE DATABASE import_slip;
+EOSQL
